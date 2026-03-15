f1eebefa80409cb9065b83ef1ab5680ede663da4
kartikeyansundaresan <skartikeyan121999@gmail.com>
2026-03-15 10:55:46 +0530

    Improving unit and integration  tests

--- src/test/java/com/gondorgates/limiter_service/LimiterServiceApplicationTests.java
+++ src/test/java/com/gondorgates/limiter_service/LimiterServiceApplicationTests.java
@@ -2,12 +2,18 @@ package com.gondorgates.limiter_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.time.Clock;
 
 @SpringBootTest
 class LimiterServiceApplicationTests {
 
+	// This "fakes" the clock bean so the ApplicationContext can start successfully
+	@MockBean
+	private Clock clock;
+
 	@Test
 	void contextLoads() {
+		// This test will now pass if the app starts without errors
 	}
-
 }
